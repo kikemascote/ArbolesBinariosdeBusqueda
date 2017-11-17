74752448cc34625f4ccd1293159889eb09b227fa
kike Mascote <kike.Mascote@gmail.com>
2017-11-17 08:07:56 -0700

    gsdf

---
+++ src/mx/maxa/node/node.java
@@ -0,0 +1,55 @@
+package mx.maxa.node;
+
+public class node<T> implements Comparable<T>{
+    private T       value = null;
+    private node<T> left = null;
+    private node<T> right = null;
+    private long    index = 0;
+
+    public long getIndex() {
+        return index;
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public node() {
+
+        this.value = null;
+
+    }
+
+    public node(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(node<T> left) {
+        this.left = left;
+    }
+
+    public node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(node<T> right) {
+        this.right = right;
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return 0;
+    }
+}
