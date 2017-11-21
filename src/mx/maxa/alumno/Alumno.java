869cc96319fde19dfefaa9273402d4094b9a905a
kike Mascote <kike.Mascote@gmail.com>
2017-11-21 13:44:36 -0700

    poquito mas

--- src/mx/maxa/alumno/Alumno.java
+++ src/mx/maxa/alumno/Alumno.java
@@ -5,11 +5,32 @@ import java.util.Comparator;
 public class Alumno implements Comparable<Alumno>, Comparator<Alumno> {
     String nombre=null;
     Double promedio=0.0d;
+
+
+
     public Alumno(){
-        
+        nombre="";
+        promedio=0d;
+
     }
 
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(Double promedio) {
+        this.promedio = promedio;
+    }
+    
     @Override
     public int compareTo(Alumno o) {
         return 0;
